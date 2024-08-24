a52740c97c99c84044a86bd607935fe6a5158e9f
shivam <shiva2641998@gmail.com>
2024-08-24 22:00:56 +0530

    theme api issue fixed

--- app/routes/themeRoutes.js
+++ app/routes/themeRoutes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const themeController = require('../controllers/themeController.js');
 
-router.post('/theme', themeController.createData);
-router.get('/theme/:id', themeController.getDataById);
-router.put('/theme/:id', themeController.updateDataById);
-router.delete('/theme/:id', themeController.deleteDataById);
-router.get('/theme', themeController.getAllData);
+router.post('/', themeController.createData);
+router.get('/:id', themeController.getDataById);
+router.put('/:id', themeController.updateDataById);
+router.delete('/:id', themeController.deleteDataById);
+router.get('/', themeController.getAllData);
 
 module.exports = router;
