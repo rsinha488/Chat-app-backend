3d2cdcbc12c62fb737388a64c6c54c7705c0737c
Ruchi Sinha <rsinha488@gmail.com>
2024-10-01 12:57:39 +0530

    ads status

--- app/routes/advertisementRoute.js
+++ app/routes/advertisementRoute.js
@@ -5,7 +5,7 @@ const advertisementController = require("../controllers/advertisementController"
 router.post("/", advertisementController.createAdvertisement);
 router.get("/", advertisementController.getAllAdvertisements);
 
-// router.get("/status", advertisementController.getAllAdvertisements);
+router.get("/status=:status", advertisementController.getAllAdvertisements);
 router.get("/:id", advertisementController.getAdvertisementDetail);
 router.delete("/:id", advertisementController.softDeleteAdvertisement);
 router.put("/:id", advertisementController.updateAdvertisement);
