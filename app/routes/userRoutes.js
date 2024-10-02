d1bf41d7002d6c04709c7cafa2ac3f41d8f29ccc
Ruchi Sinha <rsinha488@gmail.com>
2024-10-02 19:21:10 +0530

    profile

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -1,38 +1,38 @@
 // userRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require("../controllers/userController");
 
-router.get('/', userController.getUser);
+router.get("/", userController.getUser);
 
-router.post('/', userController.createUser);
+router.post("/", userController.createUser);
 
-router.post('/login',userController.userLogin);
+router.post("/login", userController.userLogin);
 
-router.get('/:userId', userController.getUserDetail);
+router.get("/:userId", userController.getUserDetail);
 
-router.put('/:userId', userController.updateUserDetail);
+router.put("/:userId", userController.updateUserDetail);
 
-router.post('/subscribe', userController.subscribeToRoom);
+router.post("/subscribe", userController.subscribeToRoom);
 
-router.post('/subscribe/event', userController.subscribeToEvent);
+router.post("/subscribe/event", userController.subscribeToEvent);
 
-router.post('/subscribe/hashtag', userController.subscribeToHashTag);
+router.post("/subscribe/hashtag", userController.subscribeToHashTag);
 
-router.post('/sendFriendRequest',userController.sendFriendRequest);
+router.post("/sendFriendRequest", userController.sendFriendRequest);
 
-router.post ('/acceptFriendRequest',userController.acceptFriendRequest);
+router.post("/acceptFriendRequest", userController.acceptFriendRequest);
 
-router.post('/blockUser',userController.blockUser);
+router.post("/blockUser", userController.blockUser);
 
-router.get('/friends/:userId',userController.getFriendList);
+router.get("/friends/:userId", userController.getFriendList);
 
-router.get('/requests/:userId',userController.getFriendRequests);
+router.get("/requests/:userId", userController.getFriendRequests);
 
-router.post('/rejectFriendRequest',userController.rejectFriendRequest);
+router.post("/rejectFriendRequest", userController.rejectFriendRequest);
 
-router.post('/unblockUser',userController.unblockUser)
+router.post("/unblockUser", userController.unblockUser);
 
-
+router.get("/profile/:userId", userController.getUserProfile);
 
 module.exports = router;
