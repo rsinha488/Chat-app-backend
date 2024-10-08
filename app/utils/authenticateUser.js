f58e9ed8ccb09c847901d99458926697ef55b458
shivam <shiva2641998@gmail.com>
2024-10-08 11:40:07 +0530

    jwt verify finish

--- app/utils/authenticateUser.js
+++ app/utils/authenticateUser.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'ruchi_jwt_secret'; 
 
@@ -13,6 +14,29 @@ exports.authenticate = (req, res, next) => {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded; 
     next(); 
+  } catch (error) {
+    res.status(401).json({ message: 'Invalid or expired token' });
+  }
+};
+
+exports.verifytoken = async (req, res) => {
+  const { token } = req.body;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Access token is required' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    const user = await User.findOne({_id : decoded.userId})
+    res.status(200).json({
+      success: true,
+      data: {
+        user: user,
+        token: token
+      },
+      status: true,
+    });
   } catch (error) {
     res.status(401).json({ message: 'Invalid or expired token' });
   }
